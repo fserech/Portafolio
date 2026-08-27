b3435a9fdb0c0cb365ccb843b7faa5e448ed1a57
fserech <serech63@gmail.com>
2026-08-27 15:22:21 -0600

    visualizacion de mi curriculum

--- portfolio-angular/src/app/components/hero/hero.ts
+++ portfolio-angular/src/app/components/hero/hero.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule, ArrowRight, Github, Linkedin, Mail, Code2, LucideIconData, ShieldCheck } from 'lucide-angular';
+import { Eye, LucideAngularModule, ArrowRight, Github, Linkedin, Mail, Code2, LucideIconData, ShieldCheck } from 'lucide-angular';
+
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, ],
   templateUrl: './hero.html',
   styleUrls: ['./hero.scss']
 })
@@ -15,4 +17,6 @@ export class HeroComponent {
   readonly Mail       = Mail;
   readonly Code2      = Code2;
   readonly ShieldCheck = ShieldCheck;
+  readonly Eye         = Eye;
+  readonly cvUrl = 'https://drive.google.com/file/d/1Om2ADXG8zJvoqQq7SadWXBku6VHT2_GX/view?usp=sharing';
 }
